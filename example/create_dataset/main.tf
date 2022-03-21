3dfd1ee4c45d9b24bf3996951a3aa41373de5738
Methee Leedhanachoke <methee@honestbank.com>
2022-03-21 21:54:30 +0700

    feat: add GCP BigQuery dataset module (#2)
    * feat: add GCP BigQuery dataset module
    
    * move example inside of folder

---
+++ example/create_dataset/main.tf
@@ -0,0 +1,26 @@
+terraform {
+  required_providers {
+    google = {
+      source  = "hashicorp/google"
+      version = "~> 4.13.0"
+    }
+    time = {
+      source  = "hashicorp/time"
+      version = "0.7.2"
+    }
+    random = {
+      source  = "hashicorp/random"
+      version = "3.1.2"
+    }
+  }
+}
+
+module "crate_bigquery_dataset" {
+  source      = "../../modules/gcp_bigquery_dataset"
+  name        = "dataset"
+  description = "dataset's description"
+  location    = "asia-southeast2"
+
+  owner_email    = "poc-bigquery@poc-bigquery-343507.iam.gserviceaccount.com"
+  google_project = var.google_project
+}
---
+++ modules/gcp_bigquery_dataset/outputs.tf
@@ -0,0 +1,7 @@
+output "id" {
+  value = google_bigquery_dataset.google_bigquery_dataset.dataset_id
+}
+
+output "self_link" {
+  value = google_bigquery_dataset.google_bigquery_dataset.self_link
+}
