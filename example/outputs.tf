3f3ba28b738253cade9db64cb7f2923f066152fc
Methee Leedhanachoke <methee@honestbank.com>
2022-03-21 17:52:51 +0700

    feat: add GCP BigQuery dataset module

---
+++ example/outputs.tf
@@ -0,0 +1,7 @@
+output "bigquery_dataset_id" {
+  value = module.crate_bigquery_dataset.id
+}
+
+output "bigquery_dataset_link" {
+  value = module.crate_bigquery_dataset.self_link
+}
---
+++ test/gcp_bigquery_dataset_test.go
@@ -0,0 +1,43 @@
+package test
+
+import (
+	"context"
+	"os"
+	"testing"
+
+	"cloud.google.com/go/bigquery"
+	"github.com/gruntwork-io/terratest/modules/terraform"
+	testStructure "github.com/gruntwork-io/terratest/modules/test-structure"
+	"github.com/stretchr/testify/assert"
+	"google.golang.org/api/option"
+)
+
+func getOptions(t *testing.T, directory string) *terraform.Options {
+	return terraform.WithDefaultRetryableErrors(t, &terraform.Options{
+		TerraformDir: directory,
+	})
+}
+
+func TestGCPBigQueryDataset(t *testing.T) {
+	projectName := os.Getenv("TF_VAR_google_project")
+	credentials := os.Getenv("TF_VAR_google_credentials")
+
+	t.Run("success", func(t *testing.T) {
+		options := getOptions(t, testStructure.CopyTerraformFolderToTemp(t, "..", "example"))
+
+		defer terraform.Destroy(t, options)
+
+		terraform.InitAndApply(t, options)
+
+		id := terraform.Output(t, options, "bigquery_dataset_id")
+		assert.NotEmpty(t, id)
+		link := terraform.Output(t, options, "bigquery_dataset_link")
+		assert.NotEmpty(t, link)
+
+		ctx := context.Background()
+		client, err := bigquery.NewClient(ctx, projectName, option.WithCredentialsJSON([]byte(credentials)))
+		assert.NoError(t, err)
+
+		assert.Equal(t, projectName, client.Dataset(id).ProjectID)
+	})
+}
