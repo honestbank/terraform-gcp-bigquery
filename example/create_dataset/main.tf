015101355f75f51a7e4fa8c1a360d7415ba18903
Methee Leedhanachoke <methee@honestbank.com>
2022-03-22 10:37:37 +0700

    fix naming

--- example/create_dataset/main.tf
+++ example/create_dataset/main.tf
@@ -15,7 +15,7 @@ resource "random_id" "random_id" {
   byte_length = 4
 }
 
-resource "google_service_account" "google_service_account" {
+resource "google_service_account" "owner" {
   account_id   = "datasetowner${random_id.random_id.hex}"
   display_name = "dataset_owner"
 }
@@ -26,6 +26,6 @@ module "crate_bigquery_dataset" {
   description = "dataset's description"
   location    = "asia-southeast2"
 
-  owner_email    = google_service_account.google_service_account.email
+  owner_email    = google_service_account.owner.email
   google_project = var.google_project
 }
