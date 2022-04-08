0fd26d1004eb4f18a9586d45879012a7e6accd01
Nicholas Lu Chee Seng <nick.lu@honestbank.com>
2022-04-08 14:38:41 +0700

    feat: add bigquery cloudsql connection module

---
+++ test/gcp_bigquery_connection_test.go
@@ -0,0 +1,29 @@
+package test
+
+import (
+	"log"
+	"testing"
+
+	"github.com/gruntwork-io/terratest/modules/terraform"
+	testStructure "github.com/gruntwork-io/terratest/modules/test-structure"
+	"github.com/stretchr/testify/assert"
+)
+
+func TestGCPBigQueryConnection(t *testing.T) {
+	t.Run("success", func(t *testing.T) {
+		t.Parallel()
+
+		options := getOptions(t, testStructure.CopyTerraformFolderToTemp(t, "..", "example/create_cloudsql_connection"))
+
+		defer terraform.Destroy(t, options)
+
+		result, errorApplying := terraform.InitAndApplyE(t, options)
+		log.Print(result)
+		if errorApplying != nil {
+			return
+		}
+		// test connection based on terraform output
+		link := terraform.Output(t, options, "bigquery_connection_link")
+		assert.NotEmpty(t, link)
+	})
+}
