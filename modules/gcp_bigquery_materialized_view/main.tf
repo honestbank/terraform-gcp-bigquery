75647f40e7f2cf7fa314263dad0ddae81571668b
Methee (Art) Leedhanachoke <methee@honestbank.com>
2022-07-26 18:08:44 +0700

    fix: add missing materialized_view block (#13)

--- modules/gcp_bigquery_materialized_view/main.tf
+++ modules/gcp_bigquery_materialized_view/main.tf
@@ -11,6 +11,9 @@ resource "google_bigquery_table" "materialized_view" {
   #checkov:skip=CKV_GCP_80:Cloud KMS based encryption is not supported for views"
   dataset_id          = var.dataset_id
   table_id            = var.name
-  schema              = var.schema
   deletion_protection = var.deletion_protection
+
+  materialized_view {
+    query = var.query
+  }
 }
