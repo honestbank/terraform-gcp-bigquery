e730fa69695dedb71077795ac77e21ff87919e72
Nishchal Gautam <nishchal@honestbank.com>
2022-09-13 18:17:58 +0700

    fix: add description to the table

--- modules/gcp_bigquery_table/main.tf
+++ modules/gcp_bigquery_table/main.tf
@@ -12,6 +12,7 @@ resource "google_bigquery_table" "google_bigquery_table" {
   table_id            = var.name
   schema              = var.schema
   deletion_protection = var.deletion_protection
+  description         = var.description
 
   encryption_configuration {
     kms_key_name = var.customer_managed_key_id
