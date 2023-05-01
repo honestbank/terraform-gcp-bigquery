e5aab5e7b38484730488453b6c6df82086edb554
Thadchai Saenprasert <thadchai@honestbank.com>
2023-05-02 00:08:29 +0700

    feat: add connection id to output

--- modules/gcp_bigquery_big_lake_connection/outputs.tf
+++ modules/gcp_bigquery_big_lake_connection/outputs.tf
@@ -1,9 +1,13 @@
+output "id" {
+  description = "The reference ID of the created resource"
+  value       = google_bigquery_connection.big_lake_connection.id
+}
+output "connection_id" {
+  description = "The reference ID of the created resource"
+  value       = google_bigquery_connection.big_lake_connection.connection_id
+}
+
 output "service_account_id" {
   description = "Build-in service account that is populated after connection has been created"
   value       = google_bigquery_connection.big_lake_connection.cloud_resource[0].service_account_id
-}
-
-output "id" {
-  description = "The reference ID of the created resource"
-  value       = google_bigquery_connection.big_lake_connection.connection_id
 }
